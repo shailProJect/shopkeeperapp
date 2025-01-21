6546916e171dbfe7d83808764c6176ed542d01d7
Shailesh Choudhary <125347306+shailProJect@users.noreply.github.com>
2025-01-21 13:00:29 +0530

    Add files via upload

---
+++ shopkeeperapp/src/main/java/com/example/shopkeeperapp/ShopkeeperappApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopkeeperapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkeeperappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkeeperappApplication.class, args);
+	}
+
+}
---
+++ shopkeeperapp/src/test/java/com/example/shopkeeperapp/ShopkeeperappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopkeeperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkeeperappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
